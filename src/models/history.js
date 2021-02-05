95b2792d927e3635b927ad96f0474fe0ed5c088f
Gefyaqiilah <gefyaqiilah26@gmail.com>
2021-02-05 17:42:44 +0700

    debugging

--- src/models/history.js
+++ src/models/history.js
@@ -2,7 +2,7 @@ const { actionQuery } = require('../helpers/actionQuery')
 
 const history = {
   getOrderHistory: (userId) => {
-    return actionQuery('SELECT orderhistory.*, cart.deliveryStatus FROM orderhistory INNER JOIN cart ON cart.id = orderHistory.cartId WHERE cart.userId = ?', userId)
+    return actionQuery('SELECT orderhistory.*, cart.deliveryStatus FROM orderhistory INNER JOIN cart ON cart.id = orderhistory.cartId WHERE cart.userId = ?', userId)
   },
   deleteOrderHistory: (orderId) => {
     return actionQuery('DELETE FROM orderhistory WHERE id = ?', orderId)
